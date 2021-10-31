4429d2fc6382f2dc2298028003e373204a640e8f
inzucr <ingrid-yaluz.17@hotmail.es>
2021-10-31 12:40:55 -0500

    Layout de animales encontrados y perdidos

--- app/src/main/java/com/example/garrapp/ListadosActivity.java
+++ app/src/main/java/com/example/garrapp/ListadosActivity.java
@@ -2,25 +2,39 @@ package com.example.garrapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class ListadosActivity extends AppCompatActivity {
 
+    // barra de navegación inferior
     BottomNavigationView bottomNavigationView;
+
+    // fragmentos de la actividad
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    float v =0;
+
+    Button btn_ingreso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listados);
 
+        // fragmentos de la actividad encontrados / perdidos
+
         bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(R.id.Listados);
-
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -56,5 +70,33 @@ public class ListadosActivity extends AppCompatActivity {
 
 
 
+
+
+        // barra de navegación inferior, casos para el cambio
+
+
+        tabLayout=findViewById(R.id.tab_layoutlistados);
+        viewPager = findViewById( R.id.view_pagerlistados);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Animales perdidos"));
+        tabLayout.addTab(tabLayout.newTab().setText("Animales encontrados"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ListasAdapter adapter = new ListasAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+      //  tabLayout.setTranslationY(300);
+      //  tabLayout.setAlpha(v);
+      //  tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+
+
     }
+
+
+
 }
+
+
+
+
---
+++ app/src/main/java/com/example/garrapp/ListasAdapter.java
@@ -0,0 +1,43 @@
+package com.example.garrapp;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ListasAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+    int totalTabs;
+
+    public ListasAdapter (FragmentManager fm, Context context , int totalTabs){
+        super( fm);
+        this.context = context;
+        this.totalTabs = totalTabs;
+
+    }
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+
+
+
+    public Fragment getItem (int position){
+        switch (position){
+            case 0:
+                LoginTabFragment EncontradosTabFragment = new LoginTabFragment();
+                return EncontradosTabFragment;
+
+            case 1:
+                LoginTabFragment PerdidosTabFragment1 = new LoginTabFragment();
+                return PerdidosTabFragment1;
+            default:
+                return  null;
+        }
+
+    }
+
+
+}
