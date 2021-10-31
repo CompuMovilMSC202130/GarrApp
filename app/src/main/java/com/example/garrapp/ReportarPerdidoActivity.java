882d3fad3156e6df1c12ae13b993f8e1135a6006
inzucr <ingrid-yaluz.17@hotmail.es>
2021-10-31 18:09:56 -0500

    Layout de reporte de animales

---
+++ app/src/main/java/com/example/garrapp/ReportarPerdidoActivity.java
@@ -0,0 +1,14 @@
+package com.example.garrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportarPerdidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportar_perdido);
+    }
+}
