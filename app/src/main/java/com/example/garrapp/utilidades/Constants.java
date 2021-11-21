0fa4a381483c23e541f5b855a006ee28ffa26be4
DamianMartinez <damian.martinez@javeriana.edu.co>
2021-11-21 17:13:57 -0500

    Varios sin afectacion

--- app/src/main/java/com/example/garrapp/utilidades/Constants.java
+++ app/src/main/java/com/example/garrapp/utilidades/Constants.java
@@ -2,7 +2,7 @@ package com.example.garrapp.utilidades;
 
 public class Constants {
     public static final String KEY_COLLECTION_USERS = "users";
-    public static final String KEY_NAME = "name";
+    public static final String KEY_NAME = "names";
     public static final String KEY_EMAIL = "email";
     public static final String KEY_PASSWORD = "password";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
