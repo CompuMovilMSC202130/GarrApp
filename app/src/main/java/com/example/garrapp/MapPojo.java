deec684eddf56a63d83d4d44000f79156f00e016
diego <d.rizogallego@gmail.com>
2021-11-23 09:08:05 -0500

    Marcacion de la ruta prueba

---
+++ app/src/main/java/com/example/garrapp/MapPojo.java
@@ -0,0 +1,25 @@
+package com.example.garrapp;
+
+public class MapPojo {
+    private double latitud;
+    private double longitud;
+    private String nombre;
+
+    public MapPojo() {
+    }
+
+    public String getNombre() {
+
+        return nombre;
+    }
+
+    public double getLatitud() {
+
+        return latitud;
+    }
+
+    public double getLongitud() {
+
+        return longitud;
+    }
+}
